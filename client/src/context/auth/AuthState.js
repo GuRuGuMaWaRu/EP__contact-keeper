4a392f604c27998b0f09cb64b1d129b00da6e68e
GuRuGu <peter.k.nets@gmail.com>
2019-11-18 19:54:12 +0200

    Write preliminary loadUser function inside authstate

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -25,7 +25,17 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => console.log("load user");
+  const loadUser = async () => {
+    // @todo - load token into global headers
+
+    try {
+      const res = await axios.get("/api/auth");
+
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register User
   const register = async formData => {
